761b92b9dd0d4fc294b7ce99a2ced41f2aaf68d0
Umessi2004 <satpathiumesh.1917@gmail.com>
2024-07-17 23:50:15 +0530

    done assignment 11

--- CODING/PYTHON/assignment11.py
+++ CODING/PYTHON/assignment11.py
@@ -34,19 +34,74 @@ def question1():
     start=leftmost(l,x)
     end=rightmost(l,x)
     print([start, end])
+def question2():
+    def search(l,target):
+        ele={}
+        for i in l:
+            if i in ele:
+                ele[i]+=1
+            else:
+                ele[i]=1
+        if target in l:
+            return ele.get(target)
+        else:
+            return 0
+    n=int(input("enter the size of the list: "))
+    l=[]
+    for i in range(n):
+        xi=int(input("enter the no."))
+        l.append(xi)
+    l.sort()
+    target=int(input("enter the value to search: "))
+    val=search(l,target)
+    if val==0:
+        print("The element is not found")
+    else:
+        print(f"the ele found {val} times")
+def question3():
+    s1=input("enter first string: ")
+    s2=input("enter second string: ")
+    s1.lower()
+    s2.lower()
+    s1.replace(" ", "")
+    s2.replace(" ", "")
+    if sorted(s1)==sorted(s2):
+        print("Both are Anagram")
+    else:
+        print("No anagram")
+def question4():
+    def isprime(n):
+        c=0
+        for i in range(2, n+1):
+            if n%i==0:
+                c+=1
+        if c==1:
+            return n
+    def primecount(n):
+        c=0
+        for i in range(1, n+1):
+            if isprime(i):
+                c+=1
+        return c
+    n=int(input("enter a number: "))
+    print("the prime numbers upto the nth number is: ")
+    print(primecount(n))
 def main():
     print("ASSIGNMENT 11")
-    print("1.qustion 1\n2.question 2\n3.question 3\n")
+    print("1.qustion 1\n2.question 2\n3.question 3\n4.question4\n")
     n=int(input("enter a question number to run:"))
     if n==1:
-        print("Write a recursive function that takes a string word as input and check if the given word is palindrome. Return True if string is palindrome else return False.\n")
+        print("Given an array of integers sorted in non-decreasing order, find the starting and ending position of a given target value. If the target is not found, return [-1, -1]. \n")
         question1()
     elif n==2:
-        print("Given a list of tuples contains weight (w) and value (v) pair for items and the capacity of a bag. Write a function that calculate the maximum item value that can be carried in the bag.\n")
-        # question2()
+        print("Given an array of integers sorted in non-decreasing order, find the starting and ending position of a given target value. If the target is not found, return [-1, -1]. \n")
+        question2()
     elif n==3:
-        print("Write a recursive function that takes an integer number (num) as input and add all the digits of the number. \n")
-        # question3()
+        print("Write a function to check if two strings are anagrams of each others\n")
+        question3()
+    elif n==4:
+        print("Write a function that takes a integer n as input and calculate the number of prime numbers appears from 1 to n. \n")
+        question4()
     else:
         print("enter a valid option\n")  
 while True:
