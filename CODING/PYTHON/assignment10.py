730afcc89b4b628b17e91d87cc3d2f58274429cb
Umessi2004 <satpathiumesh.1917@gmail.com>
2024-07-17 12:43:16 +0530

    completed assignment 10 file

--- CODING/PYTHON/assignment10.py
+++ CODING/PYTHON/assignment10.py
@@ -9,14 +9,34 @@ def question1():
     s=pal(str)
     print(s)
     
-# def question2():
+def question2():
+    def max_size(items, capacity):
+        items=[(value, weight, value/weight) for value, weight in items]
+        items.sort(key=lambda x:x[2], reverse=True)
+        max_item=0.0
+        for value, weight, ratio in items:
+            if capacity>0:
+                if capacity>=weight:
+                    max_item+=value   
+                    capacity-=weight
+                else:
+                    max_item+=ratio*capacity
+            else:
+                break
+        return max_item
+    items = [(60, 10), (100, 20), (120, 30)] 
+    capacity = 50
+    print("the maximum item that can be stored is: ")
+    val=max_size(items, capacity)
+    print(val)
 def question3():
     num=int(input("Enter a number:"))
     def add(num):
         if num%10==0:
             return num
         else:
-            return num + add(num//10)
+            return num%10 + add(num//10)
+    print(f"the sum of every digit of {num} is: ")
     sum=add(num)
     print(sum)
 def main():
@@ -24,13 +44,13 @@ def main():
     print("1.qustion 1\n2.question 2\n3.question 3\n")
     n=int(input("enter a question number to run:"))
     if n==1:
-        print("Given a list of products, print out the name of all the products with a price higher than 10.\n")
+        print("Write a recursive function that takes a string word as input and check if the given word is palindrome. Return True if string is palindrome else return False.\n")
         question1()
     elif n==2:
-        print("Take two list of numbers the products quantities and the prices. Calculate total price per product. (Use list comprehension)\n")
-        # question2()
+        print("Given a list of tuples contains weight (w) and value (v) pair for items and the capacity of a bag. Write a function that calculate the maximum item value that can be carried in the bag.\n")
+        question2()
     elif n==3:
-        print("Write a function which takes a list of votes for candidates, find out who is the winner.\n")
+        print("Write a recursive function that takes an integer number (num) as input and add all the digits of the number. \n")
         question3()
     else:
         print("enter a valid option\n")  
