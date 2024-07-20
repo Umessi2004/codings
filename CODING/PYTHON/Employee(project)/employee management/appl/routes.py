e58d6e98d8b852807929ec442ddae233431d2448
Umessi2004 <satpathiumesh.1917@gmail.com>
2024-07-20 19:38:42 +0530

    updated the routes file with error handling

--- CODING/PYTHON/Employee(project)/employee management/appl/routes.py
+++ CODING/PYTHON/Employee(project)/employee management/appl/routes.py
@@ -9,44 +9,67 @@ employee_bp=Blueprint('employee_bp', __name__)
 def index():
     return 'WELCOME TO EMPLOYEE MANAGEMENT SYSTEM'
 
+#route to entetr the employee data 
 @employee_bp.route('/emp', methods=['POST'])
 def create():
     data=request.get_json()
-    emp=employee(id=data['id'], name=data['name'], age=data['age'], department=data['dept'])
-    db.session.add(emp)
-    db.session.commit()
-    return jsonify({'message':'employee added successfully!!'})
+    if not data:
+        return jsonify({'error': 'The request payload is not in JSON format'}), 415
+    try:
+        emp=employee(id=data['id'], name=data['name'], age=data['age'], department=data['dept'])
+        db.session.add(emp)
+        db.session.commit()
+        return jsonify({'message':'employee added successfully!!'})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
 
+#route to get all the employees id at a time
 @employee_bp.route('/emp', methods=['GET'])
 def get_emp():
     emp_dat = employee.query.all()
     output = []
-    for i in emp_dat:
-        emp_data={'id=':i.id, 'name=':i.name, 'age=':i.age, 'department=':i.department}
-        output.append(emp_data)
-    return jsonify({'employees:':output})
+    try:
+        for i in emp_dat:
+            emp_data={'id=':i.id, 'name=':i.name, 'age=':i.age, 'department=':i.department}
+            output.append(emp_data)
+        return jsonify({'employees:':output})
+    except Exception as e:
+        return jsonify({'error':str(e)}), 500
 
+#route to get the details of a particular employee
 @employee_bp.route('/emp<int:id>', methods=['GET'])
 def get_empid(id):
     emp_dat=employee.query.get_or_404(id)
-    # output=[]
-    emp_data={'id': emp_dat.id, 'name':emp_dat.name, 'age':emp_dat.age, 'dept':emp_dat.department}
-    # output.append(emp_data)
-    return jsonify({'particular employee data:': emp_data})
-
+    try:
+        emp_data={'id': emp_dat.id, 'name':emp_dat.name, 'age':emp_dat.age, 'dept':emp_dat.department}
+        return jsonify({'particular employee data:': emp_data})
+    except Exception as e:
+        return jsonify({'error':str(e)}), 500
+        
+#route to update the data of a particular employee
 @employee_bp.route('/emp<int:id>', methods=['PUT'])
 def update_emp(id):
     data=request.get_json()
-    emp_dat=employee.query.get_or_404(id)
-    emp_dat.name=data['name']
-    emp_dat.age=data['age']
-    emp_dat.department=data['dept']
-    db.session.commit()
-    return jsonify({'employee': 'employee data updated!!'})
+    if not data:
+        return jsonify({'error': 'The request payload is not in JSON format'}), 415
+    try:
+        emp_dat=employee.query.get_or_404(id)
+        emp_dat.name=data['name']
+        emp_dat.age=data['age']
+        emp_dat.department=data['dept']
+        db.session.commit()
+        return jsonify({'employee': 'employee data updated!!'})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
 
 @employee_bp.route('/emp<int:id>', methods=['DELETE'])
 def delete_employee(id):
-    emp_data =employee.query.get_or_404(id)
-    db.session.delete(emp_data)
-    db.session.commit()
-    return jsonify({'message': 'Employee deleted'})
+    try:
+        emp_data =employee.query.get_or_404(id)
+        db.session.delete(emp_data)
+        db.session.commit()
+        return jsonify({'message': 'Employee deleted'})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+        
